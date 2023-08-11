2117cc760f4923383cff5553d7b1d31c75930622
Malcolm Kass <45127663+malcolmkass@users.noreply.github.com>
2023-08-11 18:50:11 -0500

    Add files via upload

---
+++ rundirectory.bat
@@ -0,0 +1,12 @@
+*** rundirectory.bat ***
+
+title batch script
+cd C:\Program Files\Stata17
+
+cd C:\Users\mkass\IT (30) Advanced Dropbox\Malcolm Kass\Research\Immigration_EF_Nazmul\datafiles\csv files
+"C:\Program Files\Stata17\StataSE-64" /b do "C:\Users\mkass\IT (30) Advanced Dropbox\Malcolm Kass\Research\Immigration_EF_Nazmul\datafiles\controls_to_dta_v5.do"
+exit
+
+
+
+start StataSE-64.exe
